cef21d7224f6c6c745dc7b7e42f48a9afb1f6fdc
Dulguun0314 <Dulguunbuyannemeh0314@gmail.com>
2024-09-02 16:26:30 +0800

    last commit

--- api/src/index.js
+++ api/src/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(authMiddleware);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use("/records", recordsRouter);
 app.use("/iconcategories", iconCategoryRouter);
